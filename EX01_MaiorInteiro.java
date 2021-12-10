6180d5e85a665a7475119291108f1c8301521205
Tiago Teixeira <54964615+tsteix@users.noreply.github.com>
2021-12-09 23:56:45 -0300

    Arquivos Bootcamp 09/12

---
+++ EX01_MaiorInteiro.java
@@ -0,0 +1,35 @@
+package br.com.generation.condicionais;
+import java.util.Scanner;
+
+
+public class EX01_MaiorInteiro {
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner (System.in);
+		int n1, n2, n3;
+		int maiorNum = 01;
+		
+		System.out.println("Digite o primeiro numero: ");
+		n1 = entrada.nextInt();
+		
+		System.out.println("Digite o segundo numero: ");
+		n2 = entrada.nextInt();
+		
+		System.out.println("Digite o terceiro numero: ");
+		n3 = entrada.nextInt();
+
+		if (n1 > maiorNum) {
+			maiorNum = n1;		
+		}
+		if (n2 > maiorNum) {
+			maiorNum = n2;	
+		}
+		if (n3 > maiorNum) {
+			maiorNum = n3;	
+		}
+				
+		System.out.println("O maior numero digitado é: " + maiorNum);
+		
+	}
+
+}
